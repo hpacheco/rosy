80e054499824e0a1f0749618d2544587998c3a80
Hugo Pacheco <hpacheco@gmail.com>
2021-02-20 18:55:05 +0000

    spawn test

--- src/Rosy/Robot/Turtlesim/Core.hs
+++ src/Rosy/Robot/Turtlesim/Core.hs
@@ -171,7 +171,8 @@ spawnService w req@(SpawnRequest x y o name) = do
     case mb of 
         Nothing -> return $ SpawnResponse ""
         Just r -> do
-            flushTopic ("turtle" ++ show (_robotId r) </> "pose")
+            liftIO $ threadDelay 1000000 -- wait a second before flushing
+            flushTopic ("turtle" ++ show (_robotId r) </> "pose") 
             name' <- liftIO $ spawnRobotState (Pose x y o 0 0) name r
             return $ SpawnResponse name'
 
